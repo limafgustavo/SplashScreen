808b2d2d54967e1e166ebfa62fe9717685c75abe
Gustavo Lima <gustavo.f12@hotmail.com>
2023-06-16 17:53:38 -0300

    View Binding implemented

--- app/src/main/java/com/example/splashscreen/Home.kt
+++ app/src/main/java/com/example/splashscreen/Home.kt
@@ -2,10 +2,18 @@ package com.example.splashscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.splashscreen.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
+    private lateinit var binding:ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonTest.setOnClickListener {
+            binding.textHome.text = "Working!"
+        }
+
     }
 }
--- app/src/main/java/com/example/splashscreen/MainActivity.kt
+++ app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
